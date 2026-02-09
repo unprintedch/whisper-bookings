173ce50ccc85564b6e27307bb95664626671872f
dc <dc@unprinted.ch>
2026-02-09 09:57:12 +0000

    File changes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 
 export default function IndexPage() {
-  // Redirect directly to Dashboard page
-  return <Navigate to="/Dashboard" replace />;
+  // Redirect directly to Home page
+  return <Navigate to="/Home" replace />;
 }
