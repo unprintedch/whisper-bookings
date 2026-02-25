95bbbbaabae05e5a373dbe2d553c853ef66d0687
dc <dc@unprinted.ch>
2026-02-25 15:36:54 +0000

    File changes

--- src/components/dashboard/GanttChart.jsx
+++ src/components/dashboard/GanttChart.jsx
@@ -407,10 +407,20 @@ export default function GanttChart({
                         const COL_WIDTH = 120;
                         const HALF_COL_WIDTH = COL_WIDTH / 2;
 
-                        // Midday-to-midday: start at middle of check-in day, end at middle of check-out day
-                        const startPixel = position.startIndex * COL_WIDTH + HALF_COL_WIDTH;
-                        const endPixel = position.endIndex * COL_WIDTH + HALF_COL_WIDTH;
-                        const widthPixel = endPixel - startPixel;
+                        let startPixel;
+                        if (position.startsBefore) {
+                          startPixel = position.startIndex * COL_WIDTH;
+                        } else {
+                          startPixel = position.startIndex * COL_WIDTH + HALF_COL_WIDTH;
+                        }
+
+                        let widthPixel;
+                        if (position.endsAfter) {
+                          widthPixel = position.endIndex * COL_WIDTH - startPixel;
+                        } else {
+                          const endPixel = position.endIndex * COL_WIDTH + HALF_COL_WIDTH;
+                          widthPixel = endPixel - startPixel;
+                        }
 
                         const adults = position.reservation.adults_count || 0;
                         const children = position.reservation.children_count || 0;
@@ -439,7 +449,7 @@ export default function GanttChart({
                             }}
                             onClick={(e) => handleBookingClick(position.reservation, e)}>
 
-                            <div className="absolute inset-y-1 w-full flex flex-col justify-center relative rounded px-2 py-1  opacity-40 h-full"
+                            <div className="absolute inset-y-1 w-full flex flex-col justify-center relative rounded px-2 py-1 h-full"
 
 
 
